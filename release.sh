a78b6f83a65ae705a8a3639f03f76133f92d9232
Moose <jonathan.moss@tangentsnowball.com.au>
2013-03-14 08:50:07 +1100

    Getting ready for PyPI release

---
+++ release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+RELEASE_NUM=`./setup.py --version`
+git tag | grep $RELEASE_NUM > /dev/null && \
+echo "New version number required ($RELEASE_NUM already used)" && exit 1
+
+# Push to PyPi
+./setup.py sdist upload
+
+# Tag in Git
+git tag $RELEASE_NUM -m "Tagging release $RELEASE_NUM"
+git push --tags
+git push
