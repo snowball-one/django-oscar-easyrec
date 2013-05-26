0e8325b8122040529711c1437793d1ac524eb41c
Moose <jonathan.moss@tangentsnowball.com.au>
2013-05-27 09:37:45 +1000

    updating push to master

--- release.sh
+++ release.sh
@@ -10,4 +10,4 @@ echo "New version number required ($RELEASE_NUM already used)" && exit 1
 # Tag in Git
 git tag $RELEASE_NUM -m "Tagging release $RELEASE_NUM"
 git push origin --tags
-git push origin
+git push origin master
